2945b5baf6dd83f3f6ee56355af922f116fed88e
chip2n <andreas@arvidsson.io>
2022-12-31 18:01:24 +0100

    Skip storing allocator for Material structs

--- src/mtl.zig
+++ src/mtl.zig
@@ -4,10 +4,10 @@ const parseFloat = std.fmt.parseFloat;
 const parseInt = std.fmt.parseInt;
 
 pub const MaterialData = struct {
-    materials: std.StringHashMap(Material),
+    materials: std.StringHashMapUnmanaged(Material),
 
-    pub fn deinit(self: *@This()) void {
-        self.materials.deinit();
+    pub fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+        self.materials.deinit(allocator);
     }
 };
 
@@ -44,7 +44,7 @@ const Keyword = enum {
 };
 
 pub fn parse(allocator: std.mem.Allocator, data: []const u8) !MaterialData {
-    var materials = std.StringHashMap(Material).init(allocator);
+    var materials = std.StringHashMapUnmanaged(Material){};
 
     var lines = std.mem.tokenize(u8, data, "\r\n");
     var current_material = Material{};
@@ -58,7 +58,7 @@ pub fn parse(allocator: std.mem.Allocator, data: []const u8) !MaterialData {
             .comment => {},
             .new_material => {
                 if (name) |n| {
-                    try materials.put(n, current_material);
+                    try materials.put(allocator, n, current_material);
                     current_material = Material{};
                 }
                 name = words.next().?;
@@ -99,7 +99,7 @@ pub fn parse(allocator: std.mem.Allocator, data: []const u8) !MaterialData {
         }
     }
 
-    try materials.put(name.?, current_material);
+    try materials.put(allocator, name.?, current_material);
 
     return MaterialData{ .materials = materials };
 }
@@ -153,7 +153,7 @@ test "single material" {
     const data = @embedFile("../examples/single.mtl");
 
     var result = try parse(test_allocator, data);
-    defer result.deinit();
+    defer result.deinit(test_allocator);
 
     const material = result.materials.get("Material").?;
     try expectEqualSlices(f32, &material.ambient_color.?, &.{ 1.0, 1.0, 1.0 });
@@ -173,7 +173,7 @@ test "windows line endings" {
     const data = @embedFile("../examples/triangle_windows.mtl");
 
     var result = try parse(test_allocator, data);
-    defer result.deinit();
+    defer result.deinit(test_allocator);
 
     const material = result.materials.get("None").?;
     try expectEqualSlices(f32, &material.ambient_color.?, &.{ 0.8, 0.8, 0.8 });
