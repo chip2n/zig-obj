6ffcd93e853a72f98770dbc4c2ce33f93eb6121d
chip2n <andreas@arvidsson.io>
2022-12-31 17:51:48 +0100

    Update README with better build and usage instructions

--- build.zig
+++ build.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const Mode = std.builtin.Mode;
 
+pub const pkg = std.build.Pkg{
+    .name = "zig-obj",
+    .source = .{ .path = rootDir() ++ "/src/main.zig" },
+};
+
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("zig-obj", "src/main.zig");
@@ -16,4 +21,8 @@ pub fn build(b: *Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+}
+
+inline fn rootDir() []const u8 {
+    return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
