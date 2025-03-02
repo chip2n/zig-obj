e33ed5e78171050047ba6ac0450cfdea17887e49
chip2n <andreas@arvidsson.io>
2025-03-02 07:45:58 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig_overlay.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig_overlay, ...}:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      zig = zig_overlay.packages.${system}.master-2025-03-01;
+    in {
+      devShell = pkgs.mkShell {
+        packages = [
+          zig
+        ];
+        shellHook = ''
+          echo "zig $(zig version)"
+        '';
+      };
+    });
+}
